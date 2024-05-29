4141588972f9b602a4fe90eb4763759db578d983
BMDK9 <qnrakdlzm77@naver.com>
2024-05-29 20:09:52 +0900

    #2 feat : WriteDay Repository

---
+++ src/main/java/com/example/gagyebu/domain/WriteDay/repository/WriteDayRepository.java
@@ -0,0 +1,8 @@
+package com.example.gagyebu.domain.WriteDay.repository;
+
+import com.example.gagyebu.domain.WriteDay.entity.WriteDay;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WriteDayRepository extends JpaRepository<WriteDay, Long> {
+
+}
