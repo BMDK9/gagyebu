62a814b9975b79e8ca34a67498b4b792d22ff1f4
BMDK9 <qnrakdlzm77@naver.com>
2024-05-29 20:10:07 +0900

    #2 feat : WriteDay Controller

---
+++ src/main/java/com/example/gagyebu/domain/WriteDay/controller/WriteDayController.java
@@ -0,0 +1,31 @@
+package com.example.gagyebu.domain.WriteDay.controller;
+
+import com.example.gagyebu.domain.WriteDay.dto.request.CreateWriteDayRequestDto;
+import com.example.gagyebu.domain.WriteDay.dto.response.CreateWriteDayResponseDto;
+import com.example.gagyebu.domain.WriteDay.service.WriteDayService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/writeday")
+public class WriteDayController {
+
+    private final WriteDayService writeDayService;
+
+    @PostMapping
+    public ResponseEntity<CreateWriteDayResponseDto> createWriteDay(
+            @RequestBody CreateWriteDayRequestDto req) {
+
+        CreateWriteDayResponseDto res = writeDayService.createWriteDay(req);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(res);
+    }
+}
