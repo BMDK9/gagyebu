8c932ab19dc57f30192bcdce06bea893471385a4
BMDK9 <qnrakdlzm77@naver.com>
2024-06-12 23:11:43 +0900

    #2 feat : WriteDayService - getWriteDay

--- src/main/java/com/example/gagyebu/domain/WriteDay/service/WriteDayService.java
+++ src/main/java/com/example/gagyebu/domain/WriteDay/service/WriteDayService.java
@@ -1,9 +1,12 @@
 package com.example.gagyebu.domain.WriteDay.service;
 
+import com.example.gagyebu.domain.WriteDay.dto.GetWriteDayResponseDto;
 import com.example.gagyebu.domain.WriteDay.dto.request.CreateWriteDayRequestDto;
 import com.example.gagyebu.domain.WriteDay.dto.response.CreateWriteDayResponseDto;
 
 public interface WriteDayService {
 
     CreateWriteDayResponseDto createWriteDay(CreateWriteDayRequestDto req);
+
+    GetWriteDayResponseDto getWriteDay(Long writeDayId);
 }
--- src/main/java/com/example/gagyebu/domain/WriteDay/service/impl/WriteDayServiceImplVer1.java
+++ src/main/java/com/example/gagyebu/domain/WriteDay/service/impl/WriteDayServiceImplVer1.java
@@ -1,13 +1,16 @@
 package com.example.gagyebu.domain.WriteDay.service.impl;
 
+import com.example.gagyebu.domain.WriteDay.dto.GetWriteDayResponseDto;
 import com.example.gagyebu.domain.WriteDay.dto.request.CreateWriteDayRequestDto;
 import com.example.gagyebu.domain.WriteDay.dto.response.CreateWriteDayResponseDto;
 import com.example.gagyebu.domain.WriteDay.entity.WriteDay;
 import com.example.gagyebu.domain.WriteDay.repository.WriteDayRepository;
 import com.example.gagyebu.domain.WriteDay.service.WriteDayService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class WriteDayServiceImplVer1 implements WriteDayService {
@@ -24,6 +27,21 @@ public class WriteDayServiceImplVer1 implements WriteDayService {
 
         writeDayRepository.save(writeDay);
 
+        log.info("id = {}, month = {}, date = {}",
+                writeDay.getId(), writeDay.getMonth(), writeDay.getDate());
+
         return CreateWriteDayResponseDto.of(writeDay);
+    }
+
+    @Override
+    public GetWriteDayResponseDto getWriteDay(Long writeDayId) {
+
+        WriteDay writeDay = writeDayRepository.findById(writeDayId).orElseThrow(
+                () -> new IllegalArgumentException("해당 날짜 가계부 없음"));
+
+        log.info("id = {}, month = {}, date = {}",
+                writeDay.getId(), writeDay.getMonth(), writeDay.getDate());
+
+        return  GetWriteDayResponseDto.of(writeDay);
     }
 }
