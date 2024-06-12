d45b65f55ba7d513c9a2d3cdfaf5e8889f51fbe7
BMDK9 <qnrakdlzm77@naver.com>
2024-06-12 23:11:30 +0900

    #2 feat : WriteDayController - getWriteDay

--- src/main/java/com/example/gagyebu/domain/WriteDay/controller/WriteDayController.java
+++ src/main/java/com/example/gagyebu/domain/WriteDay/controller/WriteDayController.java
@@ -1,5 +1,6 @@
 package com.example.gagyebu.domain.WriteDay.controller;
 
+import com.example.gagyebu.domain.WriteDay.dto.GetWriteDayResponseDto;
 import com.example.gagyebu.domain.WriteDay.dto.request.CreateWriteDayRequestDto;
 import com.example.gagyebu.domain.WriteDay.dto.response.CreateWriteDayResponseDto;
 import com.example.gagyebu.domain.WriteDay.service.WriteDayService;
@@ -7,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +30,12 @@ public class WriteDayController {
         CreateWriteDayResponseDto res = writeDayService.createWriteDay(req);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(res);
+    }
+
+    @GetMapping
+    public  ResponseEntity<GetWriteDayResponseDto> getWriteDay(@PathVariable Long writeDayId) {
+
+        GetWriteDayResponseDto res = writeDayService.getWriteDay(writeDayId);
+        return ResponseEntity.ok(res);
     }
 }
