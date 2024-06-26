a5fd57d2ad696c990ca922b707dfb34de9b1de94
BMDK9 <qnrakdlzm77@naver.com>
2024-06-26 22:45:05 +0900

    #2 refactor : CreateWriteDayRequestDto 날짜 수정 기능 추가

--- src/main/java/com/example/gagyebu/domain/WriteDay/dto/request/CreateWriteDayRequestDto.java
+++ src/main/java/com/example/gagyebu/domain/WriteDay/dto/request/CreateWriteDayRequestDto.java
@@ -7,4 +7,14 @@ public class CreateWriteDayRequestDto {
 
     private Integer month;
     private Integer date;
+
+    public void updateMonth (int month) {
+
+        this.month = month;
+    }
+
+    public void updateDate (int date) {
+
+        this.date = date;
+    }
 }
